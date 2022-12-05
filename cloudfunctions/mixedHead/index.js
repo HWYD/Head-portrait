1c3184acd0922e38e1ef096ded93159312ebed95
weiyue.huang <weiyue.huang@adspower.net>
2022-12-05 18:12:46 +0800

    优化

--- cloudfunctions/mixedHead/index.js
+++ cloudfunctions/mixedHead/index.js
@@ -1,11 +1,14 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-cloud.init()
+cloud.init({
+  env: 'cloud1-7gy63ub05adcdf3b'
+})
 const TcbRouter = require('tcb-router')
 const db = cloud.database()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  console.log('来了',event,context)
   const wxContext = cloud.getWXContext()
   const app =new TcbRouter({event})
   app.router('getMixedHead',async(ctx,next)=>{ 
@@ -13,11 +16,11 @@ exports.main = async (event, context) => {
     if(!sort){
      sort = 'desc'
     }
-    ctx.body= await db.collection('mixedHead').orderBy('createTime',sort)
+    ctx.body= await db.collection('photo').orderBy('createTime',sort)
     .skip(event.start)
     .limit(event.count).get()
   }) 
-  
+  console.log('出了',event,context)
   return app.serve()
   
 }
